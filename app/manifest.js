05dcdd7a75a1e7e0acc80a816045b7be287b24af
Muhammad Usman <raiusman37218@gmail.com>
2026-07-17 16:00:19 -0700

    wqduwq

--- app/manifest.js
+++ app/manifest.js
@@ -12,12 +12,12 @@ export default function manifest() {
     theme_color: "#16452c",
     icons: [
       {
-        src: absoluteUrl("/bustaniya-logo-key.png"),
+        src: absoluteUrl("/icon-192.png"),
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: absoluteUrl("/bustaniya-logo-v2-key.png"),
+        src: absoluteUrl("/icon-512.png"),
         sizes: "512x512",
         type: "image/png",
       },
